4a531a12e89c422798579cc5d54433989cecd890
Ayden Aba <abaayd01@gmail.com>
2019-04-11 20:47:53 +0930

    adding root/lib folder, deleting root/public dir

---
+++ bol-bands-algo_server/lib/FlaskAppInterface.js
@@ -0,0 +1,26 @@
+const axios = require("axios");
+
+const evaluatePrice = async price => {
+    try {
+        const response = await axios.post(
+            `${process.env.FLASK_BASE_URL}/evaluate-price`,
+            {
+                price
+            }
+        );
+
+        return {
+            success: true,
+            data: response.data
+        };
+    } catch (err) {
+        console.log(error);
+        return {
+            success: false,
+            data: null,
+            error
+        };
+    }
+};
+
+exports.evaluatePrice = evaluatePrice;
---
+++ bol-bands-algo_server/lib/PriceEvaluationTaskRunner.js
@@ -0,0 +1,33 @@
+const cryptoCompareInterface = require("@lib/CryptoCompareInterface");
+const flaskAppInterface = require("@lib/FlaskAppInterface");
+const PriceEvaluation = require("@models/PriceEvaluation");
+
+const evaluatePrice = async () => {
+    try {
+        const cryptoCompareResponse = await cryptoCompareInterface.fetchCurrentPrice();
+        const currentPrice = cryptoCompareResponse["USD"];
+        const flaskAppResponse = await flaskAppInterface.evaluatePrice(
+            currentPrice
+        );
+
+        const priceEvaluation = flaskAppResponse.data;
+
+        if (priceEvaluation !== null) {
+            newPriceEvaluation = new PriceEvaluation({ ...priceEvaluation });
+            newPriceEvaluation.save(error => {
+                if (error) {
+                    return error;
+                } else {
+                    console.log("price evaluation saved!");
+                    console.log(newPriceEvaluation);
+                }
+            });
+        } else {
+            console.log("no action");
+        }
+    } catch (err) {
+        console.log(err);
+    }
+};
+
+exports.evaluatePrice = evaluatePrice;
