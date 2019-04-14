c5d34cc79c2981b191019fda3123498631e4cc7e
Ayden <abaayd01@gmail.com>
2019-04-14 16:37:20 +0930

    graphql subscriptions client side

--- bol-bands-algo_client/src/components/PositionsTable.vue
+++ bol-bands-algo_client/src/components/PositionsTable.vue
@@ -1,91 +1,107 @@
 <template>
-	<div>
-		<v-toolbar flat>
-			<v-toolbar-title>Positions</v-toolbar-title>
-			<v-divider class="mx-4" inset vertical></v-divider>
-			<v-spacer></v-spacer>
-			<v-dialog v-model="dialog" max-width="500px">
-				<template v-slot:activator="{ on }">
-					<v-btn color="info" @click="open">New Position</v-btn>
-				</template>
-				<v-card>
-					<v-card-title>
-						<span class="headline">{{ formTitle }}</span>
-					</v-card-title>
+    <div>
+        <v-toolbar flat>
+            <v-toolbar-title>Positions</v-toolbar-title>
+            <v-divider class="mx-4" inset vertical></v-divider>
+            <v-spacer></v-spacer>
+            <v-dialog v-model="dialog" max-width="500px">
+                <template v-slot:activator="{ on }">
+                    <v-btn color="info" @click="open">New Position</v-btn>
+                </template>
+                <v-card>
+                    <v-card-title>
+                        <span class="headline">{{ formTitle }}</span>
+                    </v-card-title>
 
-					<v-card-text>
-						<v-container grid-list-md>
-							<v-datetime-picker label="Entry Date" v-model="entryDate"></v-datetime-picker>
-							<v-text-field label="Entry Price" type="number"></v-text-field>
-							<v-text-field label="Exit Price" type="number"></v-text-field>
-							<v-text-field label="Stop Loss" type="number"></v-text-field>
+                    <v-card-text>
+                        <v-container grid-list-md>
+                            <!-- <v-datetime-picker label="Entry Date" v-model="entryDate"></v-datetime-picker> -->
+                            <v-text-field label="Entry Price" type="number"></v-text-field>
+                            <v-text-field label="Exit Price" type="number"></v-text-field>
+                            <v-text-field label="Stop Loss" type="number"></v-text-field>
 
-							<span class="subheading grey--text text--darken-1">Action Type</span>
-							<v-radio-group v-model="actionType">
-								<v-radio label="BUY" value="BUY"></v-radio>
-								<v-radio label="SELL" value="SELL"></v-radio>
-							</v-radio-group>
-						</v-container>
-					</v-card-text>
+                            <span class="subheading grey--text text--darken-1">Action Type</span>
+                            <v-radio-group v-model="actionType">
+                                <v-radio label="BUY" value="BUY"></v-radio>
+                                <v-radio label="SELL" value="SELL"></v-radio>
+                            </v-radio-group>
+                        </v-container>
+                    </v-card-text>
 
-					<v-card-actions>
-						<v-spacer></v-spacer>
-						<v-btn color="blue darken-1" flat @click="close">Cancel</v-btn>
-						<v-btn color="blue darken-1" flat @click="save">Save</v-btn>
-					</v-card-actions>
-				</v-card>
-			</v-dialog>
-		</v-toolbar>
+                    <v-card-actions>
+                        <v-spacer></v-spacer>
+                        <v-btn color="blue darken-1" flat @click="close">Cancel</v-btn>
+                        <v-btn color="blue darken-1" flat @click="save">Save</v-btn>
+                    </v-card-actions>
+                </v-card>
+            </v-dialog>
+        </v-toolbar>
 
-		<v-data-table :headers="headers" :items="positions">
-			<template v-slot:items="props">
-				<td>{{ props.item.entry_date }}</td>
-				<td>{{ props.item.entry_price }}</td>
-				<td>{{ props.item.exit_price }}</td>
-				<td>{{ props.item.stop_loss }}</td>
-				<td>{{ props.item.action }}</td>
-				<td class="justify-center layout px-0">
-					<v-icon small class="mr-2" @click="editItem(props.item)">edit</v-icon>
-					<v-icon small @click="deleteItem(props.item)">delete</v-icon>
-				</td>
-			</template>
-		</v-data-table>
-	</div>
+        <v-data-table :headers="headers" :items="positions">
+            <template v-slot:items="props">
+                <td>{{ props.item.entry_date }}</td>
+                <td>{{ props.item.entry_price }}</td>
+                <td>{{ props.item.exit_price }}</td>
+                <td>{{ props.item.stop_loss }}</td>
+                <td>{{ props.item.action }}</td>
+                <td class="justify-center layout px-0">
+                    <v-icon small class="mr-2" @click="editItem(props.item)">edit</v-icon>
+                    <v-icon small @click="deleteItem(props.item)">delete</v-icon>
+                </td>
+            </template>
+        </v-data-table>
+    </div>
 </template>
 
 <script>
-	import PositionsQuery from "../graphql/Positions.gql";
+    import PositionsQuery from "../graphql/Positions.gql";
+    import CreatePositionMutation from "../graphql/PositionCreate.gql";
 
-	export default {
-		apollo: {
-			positions: PositionsQuery
-		},
-		data: () => ({
-			headers: [
-				{ text: "Entry Date", value: "entry_date" },
-				{ text: "Entry Price", value: "entry_price" },
-				{ text: "Exit Price", value: "exit_price" },
-				{ text: "Stop Loss", value: "stop_loss" },
-				{ text: "Action", value: "action" },
-				{ text: "", value: "" }
-			],
-			dialog: false,
-			formTitle: "Create New Position",
-			entryDate: Date.now(),
-			actionType: "BUY" // 1 == BUY, 2 == SELL
-		}),
-		methods: {
-			open() {
-				this.dialog = true;
-			},
-			close() {
-				this.dialog = false;
-			},
-			save() {
-				this.close();
-			}
-		}
-	};
+    export default {
+        apollo: {
+            positions: PositionsQuery
+        },
+        data: () => ({
+            headers: [
+                { text: "Entry Date", value: "entry_date" },
+                { text: "Entry Price", value: "entry_price" },
+                { text: "Exit Price", value: "exit_price" },
+                { text: "Stop Loss", value: "stop_loss" },
+                { text: "Action", value: "action" },
+                { text: "", value: "" }
+            ],
+            dialog: false,
+            formTitle: "Create New Position",
+            entryDate: Date.now(),
+            actionType: "BUY" // 1 == BUY, 2 == SELL
+        }),
+        methods: {
+            open() {
+                this.dialog = true;
+            },
+            close() {
+                this.dialog = false;
+            },
+            async save() {
+                await this.createPosition();
+                this.close();
+            },
+            async createPosition() {
+                this.$apollo.mutate({
+					mutation: CreatePositionMutation,
+					variables: {
+						input: {
+							entry_date: new Date().toISOString(),
+							entry_price: 999,
+							exit_price: 999,
+							stop_loss: 666,
+							action: "BUY"
+						}
+					}
+                });
+            }
+        }
+    };
 </script>
 
 <style scoped>
--- bol-bands-algo_client/src/main.js
+++ bol-bands-algo_client/src/main.js
@@ -1,17 +1,51 @@
-import Vue from 'vue';
-import './plugins/vuetify';
-import App from './App.vue';
-import { createProvider } from './vue-apollo';
-import Vuetify from 'vuetify';
-import DatetimePicker from 'vuetify-datetime-picker';
-import 'vuetify-datetime-picker/src/stylus/main.styl';
+import { InMemoryCache } from "apollo-cache-inmemory";
+import { ApolloClient } from "apollo-client";
+import { split } from "apollo-link";
+import { HttpLink } from "apollo-link-http";
+import { WebSocketLink } from "apollo-link-ws";
+import { getMainDefinition } from "apollo-utilities";
+import Vue from "vue";
+import VueApollo from "vue-apollo";
+import Vuetify from "vuetify";
+import App from "./App.vue";
+import 'vuetify/dist/vuetify.min.css' // Ensure you are using css-loader
 
 Vue.config.productionTip = false;
 
-new Vue({
-    apolloProvider: createProvider(),
-    render: h => h(App)
-}).$mount('#app');
+const httpLink = new HttpLink({
+    uri: "http://localhost:8080/graphql"
+});
 
+const wsLink = new WebSocketLink({
+    uri: "ws://localhost:8080/graphql",
+    options: {
+        reconnect: true
+    }
+});
+
+const link = split(
+    ({ query }) => {
+        const { kind, operation } = getMainDefinition(query);
+        return kind === "OperationDefinition" && operation === "subscription";
+    },
+    wsLink,
+    httpLink
+);
+
+const apolloClient = new ApolloClient({
+    link,
+    cache: new InMemoryCache(),
+    connectToDevTools: true
+});
+
+const apolloProvider = new VueApollo({
+    defaultClient: apolloClient
+});
+
+Vue.use(VueApollo);
 Vue.use(Vuetify);
-Vue.use(DatetimePicker);
+
+new Vue({
+    apolloProvider,
+    render: h => h(App)
+}).$mount("#app");
