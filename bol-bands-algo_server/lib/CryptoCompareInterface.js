193827ba015f1ef1aefbedc16048695df6642c94
Ayden <abaayd01@gmail.com>
2019-05-12 13:23:49 +0930

    cleanup formatting on CryptoCompareInterface file

--- bol-bands-algo_server/lib/CryptoCompareInterface.js
+++ bol-bands-algo_server/lib/CryptoCompareInterface.js
@@ -1,36 +1,35 @@
-const axios = require('axios')
-const PriceSnapshot = require('@models/PriceSnapshot')
+const axios = require('axios');
+const PriceSnapshot = require('@models/PriceSnapshot');
 
 const fetchCurrentPrice = async () => {
-  try {
-    response = await axios.get(
-      `${process.env.CRYPTO_COMPARE_URL}/price?fsym=ETH&tsyms=USD`
-    )
-    console.log(response.data)
-    return response.data
-  } catch (err) {
-    console.error(err)
-    return err
-  }
-}
-exports.fetchCurrentPrice = fetchCurrentPrice
+	try {
+		const response = await axios.get(
+			`${process.env.CRYPTO_COMPARE_URL}/price?fsym=ETH&tsyms=USD`
+		);
+		console.log(response.data);
+		return response.data
+	} catch (err) {
+		console.error(err);
+		return err;
+	}
+};
+exports.fetchCurrentPrice = fetchCurrentPrice;
 
 const takeSnapshot = async () => {
-  snapshotData = await fetchCurrentPrice()
-  const price = snapshotData['USD']
+	const snapshotData = await fetchCurrentPrice();
+	const price = snapshotData['USD'];
 
-  const newPriceSnapshot = new PriceSnapshot({
-    date: Date.now(),
-    marketcap: null,
-    price,
-    volume: null
-  })
+	const newPriceSnapshot = new PriceSnapshot({
+		date: Date.now(),
+		marketcap: null,
+		price,
+		volume: null
+	});
 
-  newPriceSnapshot.save(error => {
-    if (error) {
-      return error
-    }
-  })
-}
-
-exports.takeSnapshot = takeSnapshot
+	newPriceSnapshot.save(error => {
+		if (error) {
+			return error
+		}
+	})
+};
+exports.takeSnapshot = takeSnapshot;
